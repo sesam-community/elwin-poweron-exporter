ce05057400ce1738be41b82e13609aef78727b3a
Timur Samkharadze <timur.samkharadze@sysco.no>
2019-01-17 14:28:02 +0100

    added network link properties ref mail Tony from 17/01

--- src/main/java/io/sesam/fredrikstad/demo/models/NetworkPropertyLink.java
+++ src/main/java/io/sesam/fredrikstad/demo/models/NetworkPropertyLink.java
@@ -6,21 +6,28 @@ import com.fasterxml.jackson.annotation.JsonProperty;
  *
  * @author 100tsa
  */
-public class NetworkPropertyLink extends SesamEntity{
+public class NetworkPropertyLink extends SesamEntity {
+
     @JsonProperty("PropertyNumber")
     private String propertyNumber;
-    
+
     @JsonProperty("FeederNumber")
     private int feederNumber;
-    
+
     @JsonProperty("FeedQualityID")
     private String feederQualityID;
-    
+
     @JsonProperty("Phase")
     private int phase;
-    
+
     @JsonProperty("NetworkStatus")
     private int networkStatus;
+
+    @JsonProperty("ComponentAlias")
+    private String componentAlias;
+
+    @JsonProperty("SubQualityID")
+    private char subQualityID;
 
     public NetworkPropertyLink() {
         super();
@@ -65,8 +72,21 @@ public class NetworkPropertyLink extends SesamEntity{
     public void setNetworkStatus(int networkStatus) {
         this.networkStatus = networkStatus;
     }
-    
-    
-    
-    
+
+    public String getComponentAlias() {
+        return componentAlias;
+    }
+
+    public void setComponentAlias(String componentAlias) {
+        this.componentAlias = componentAlias;
+    }
+
+    public char getSubQualityID() {
+        return subQualityID;
+    }
+
+    public void setSubQualityID(char subQualityID) {
+        this.subQualityID = subQualityID;
+    }
+
 }

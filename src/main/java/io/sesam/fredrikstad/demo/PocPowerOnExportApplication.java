73c064745867b791066272265241b1ea47bab865
Timur Samkharadze <timur.samkharadze@sysco.no>
2019-01-23 13:56:15 +0100

    added properties for SSL set up

--- src/main/java/io/sesam/fredrikstad/demo/PocPowerOnExportApplication.java
+++ src/main/java/io/sesam/fredrikstad/demo/PocPowerOnExportApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PocPowerOnExportApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PocPowerOnExportApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PocPowerOnExportApplication.class, args);
+    }
 }

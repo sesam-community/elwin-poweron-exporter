bb6cdc2de2ebdcbd792d5c10a20f5a3346f22adf
Timur Samkharadze <timur.samkharadze@sysco.no>
2019-01-23 13:56:03 +0100

    added properties for SSL set up

--- src/main/java/io/sesam/fredrikstad/demo/AppConfig.java
+++ src/main/java/io/sesam/fredrikstad/demo/AppConfig.java
@@ -2,6 +2,7 @@ package io.sesam.fredrikstad.demo;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.core.io.Resource;
 import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Component;
 
@@ -20,4 +21,43 @@ public class AppConfig {
     public String getUrl() {
         return this.url;
     }
+
+    @Value("${client.ssl.trust-store}")
+    private Resource trustStore;
+
+    @Value("${client.ssl.trust-store-password}")
+    private String trustStorePassword;
+
+    @Value("${client.ssl.use-ssl}")
+    private boolean useSSL = false;
+
+    @Value("${client.ssl.trust-all}")
+    private boolean trustAll = false;
+
+    @Nullable
+    public Resource getTrustStore() {
+        return trustStore;
+    }
+
+    @Nullable
+    public String getTrustStorePassword() {
+        return trustStorePassword;
+    }
+
+    public boolean useSSL() {
+        return useSSL;
+    }
+
+    public boolean isTrustAll() {
+        return trustAll;
+    }
+
+    @Override
+    public String toString() {
+        return "AppConfig{" + "url=" + url + ", trustStore=" + trustStore + ", trustStorePassword=" + trustStorePassword + ", useSSL=" + useSSL + ", trustAll=" + trustAll + '}';
+    }
+    
+    
+    
+    
 }
